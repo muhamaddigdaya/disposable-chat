c3359a6f7f64d4dfca826a6d81733f2248c977c2
muhamaddigdaya <89563334+muhamaddigdaya@users.noreply.github.com>
2023-07-19 17:19:12 +0700

    input (-)

---
+++ src/app/components/ui/textarea.tsx
@@ -0,0 +1,24 @@
+import { cn } from "@/app/lib/utils"
+import * as React from "react"
+
+export interface TextareaProps
+  extends React.TextareaHTMLAttributes<HTMLTextAreaElement> {}
+
+const Textarea = React.forwardRef<HTMLTextAreaElement, TextareaProps>(
+  ({ className, ...props }, ref) => {
+    return (
+      <textarea
+      aria-multiline="true"
+        className={cn(
+          "flex stretch h-10 max-h-[40vh] w-full rounded-md border border-input bg-transparent px-3 py-2 text-md overflowring-offset-background placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50",
+          className
+        )}
+        ref={ref}
+        {...props}
+      />
+    )
+  }
+)
+Textarea.displayName = "Textarea"
+
+export { Textarea }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,24 +1,11 @@
 'use client';
-import { 
-  Sheet,
-  SheetContent,
-  SheetDescription,
-  SheetHeader,
-  SheetTitle,
-  SheetTrigger,
- } from "./components/ui/sheet"
+import { Textarea } from "@/app/components/ui/textarea"
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@48,400,0,0" />
-
-      <Sheet>
-        <SheetTrigger>
-          <span class="material-symbols-outlined">menu</span></SheetTrigger>
-        <SheetContent side='left'>
-        </SheetContent>
-      </Sheet>
+      <Textarea
+        placeholder="Type your message here..."/>
     </main>
   )
 }
