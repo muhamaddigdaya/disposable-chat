6a00507e4dc4711bacf4993448cdb909be2e7ab9
muhamaddigdaya <89563334+muhamaddigdaya@users.noreply.github.com>
2023-07-23 23:09:07 +0700

    scrollarea shadcn

--- src/app/components/ui/Chats.tsx
+++ src/app/components/ui/Chats.tsx
@@ -1,12 +1,25 @@
 import React from 'react'
 import Chatinput from './Chatinput'
+import { ScrollArea } from "@/app/components/ui/scroll-area"
+import Image from 'next/image'
+
 
 type Props = {}
 
 const Chats = (props: Props) => {
   return (
     <div className='h-full w-full h-max-full bg-gray-700 flex flex-col'>
-      <div className='h-full'></div>
+      <ScrollArea className='h-full text-slate-100 pt-8 pl-8'>
+        <div className='flex flex-row gap-x-2 items-start'>
+          <Image src="/baki.jpg" width={42} height={42} alt='pp' layout="fixed" className='rounded-full ' />
+          <div className='flex flex-col'>
+            <h2 className='font-bold'>bakihanma</h2>
+            <p className='mb-2'>hello world🤫</p>
+            <p className='mb-2'>nextjs hits hard fr</p>
+            <p className='mb-2'>tough build rh</p>
+          </div>
+        </div>
+      </ScrollArea>
       <Chatinput />
     </div>
     
