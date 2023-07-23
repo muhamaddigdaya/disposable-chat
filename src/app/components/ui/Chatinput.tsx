cc914a880ffe6730e7d2e4e66ba9184e510edf6d
muhamaddigdaya <89563334+muhamaddigdaya@users.noreply.github.com>
2023-07-24 00:52:37 +0700

    hover card animation delay

--- src/app/components/ui/Chatinput.tsx
+++ src/app/components/ui/Chatinput.tsx
@@ -19,12 +19,13 @@ const Chatinput = (props: Props) => {
           className="placeholder:text-gray-400 text-gray-50 m-0 min-h-10 max-h-[44vh] w-full resize-none border-0 bg-transparent mr-2 p-0 dark:bg-transparent pl-"
           placeholder="Type your message here..."
         />
-        <HoverCard>
+        <HoverCard openDelay={150} closeDelay={150}>
           <HoverCardTrigger>
-            <Button size="icon" type="submit" variant="appear" className='self-end bottom-0 hover:bg-gray-400 text-gray-300 hover:text-gray-100'>
+            <Button size="icon" type="submit" variant="appear" className='self-end bottom-0 text-gray-400 hover:bg-inherit hover:text-gray-100'>
               <SendHorizonal className="h-4 w-4 "/>
-            </Button></HoverCardTrigger>
-          <HoverCardContent>
+            </Button>
+            </HoverCardTrigger>
+          <HoverCardContent className='hover:cursor-default'>
             Send message
           </HoverCardContent>
         </HoverCard>
