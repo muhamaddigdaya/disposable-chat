2e2212f8c227184f7f8088798c42828a98ba767d
muhamaddigdaya <89563334+muhamaddigdaya@users.noreply.github.com>
2023-07-24 18:06:08 +0700

    scroll styling

--- src/app/components/ui/scroll-area.tsx
+++ src/app/components/ui/scroll-area.tsx
@@ -1,6 +1,5 @@
 import * as React from "react"
 import * as ScrollAreaPrimitive from "@radix-ui/react-scroll-area"
-
 import { cn } from "@/app/lib/utils"
 
 const ScrollArea = React.forwardRef<
@@ -38,7 +37,7 @@ const ScrollBar = React.forwardRef<
     )}
     {...props}
   >
-    <ScrollAreaPrimitive.ScrollAreaThumb className="relative flex-1 rounded-full bg-border" />
+    <ScrollAreaPrimitive.ScrollAreaThumb className="relative flex-1 rounded-full bg-border/10 hover:bg-border/30" />
   </ScrollAreaPrimitive.ScrollAreaScrollbar>
 ))
 ScrollBar.displayName = ScrollAreaPrimitive.ScrollAreaScrollbar.displayName
