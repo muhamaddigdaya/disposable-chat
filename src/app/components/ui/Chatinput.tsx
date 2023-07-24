0b9888736ee1b7312a3a257c7feaeebce0dd4a7d
muhamaddigdaya <89563334+muhamaddigdaya@users.noreply.github.com>
2023-07-24 18:05:36 +0700

    delay update

--- src/app/components/ui/Chatinput.tsx
+++ src/app/components/ui/Chatinput.tsx
@@ -14,12 +14,12 @@ type Props = {}
 const Chatinput = (props: Props) => {
   return (
     <div className='stretch flex bg-gray-700 p-8 pt-0 items-centerrelative bottom-0 w-full '>
-      <form className='stretch shadow-md bg-gray-600 flex px-4 py-2 items-center rounded-lg relative bottom-0 w-full'>
+      <form className='stretch  bg-gray-600 flex px-4 py-2 items-center rounded-lg relative bottom-0 w-full'>
         <TextareaAutosize
           className="placeholder:text-gray-400 text-gray-50 m-0 min-h-10 max-h-[44vh] w-full resize-none border-0 bg-transparent mr-2 p-0 dark:bg-transparent pl-"
           placeholder="Type your message here..."
         />
-        <HoverCard openDelay={150} closeDelay={150}>
+        <HoverCard openDelay={300} closeDelay={0}>
           <HoverCardTrigger>
             <Button size="icon" type="submit" variant="appear" className='self-end bottom-0 text-gray-400 hover:bg-inherit hover:text-gray-100'>
               <SendHorizonal className="h-4 w-4 "/>
