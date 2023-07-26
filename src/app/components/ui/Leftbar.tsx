befb4283968358ff02239f3705e62e1c876e7467
muhamaddigdaya <89563334+muhamaddigdaya@users.noreply.github.com>
2023-07-26 16:33:32 +0700

    sidebar content

--- src/app/components/ui/Leftbar.tsx
+++ src/app/components/ui/Leftbar.tsx
@@ -5,12 +5,7 @@ type Props = {}
 const Leftbar = (props: Props) => {
   return (
     <div className='h-full w-1/6 bg-gray-800 text-slate-100 pl-8 pt-8'>
-      <ul>
-        <li className='mb-2 hover:underline cursor-pointer'>Clear</li>
-        <li className='mb-2 hover:underline cursor-pointer'></li>
-        <li className='mb-2 hover:underline cursor-pointer'>Theme</li>
-        <li className='mb-2 hover:underline cursor-pointer'>Settings</li>
-      </ul>
+
     </div>
   )
 }
--- src/app/components/ui/Rightbar.tsx
+++ src/app/components/ui/Rightbar.tsx
@@ -4,8 +4,8 @@ type Props = {}
 
 const Rightbar = (props: Props) => {
   return (
-    <div className='h-full w-1/6 bg-gray-800'>
-        
+    <div className='h-full w-1/6 bg-gray-800 text-slate-100 p-4'>
+        <div className='p-2 hover:bg-gray-600 hover:text-slate-50 rounded-md cursor-pointer'>Clear chat</div>
     </div>
   )
 }
