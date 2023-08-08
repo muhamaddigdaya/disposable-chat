3a0a1fcad5987521a1fc2aa097d7dc66b2816ac2
MD <89563334+muhamaddigdaya@users.noreply.github.com>
2023-08-08 14:42:16 +0700

    update method

--- src/app/components/ui/Conversation.tsx
+++ src/app/components/ui/Conversation.tsx
@@ -12,33 +12,31 @@ import {
 } from "@/app/components/ui/dialog"
 import { CopyBlock } from "react-code-blocks";
 
-type Props = {username?: string}
+type Props = { username?: string }
 
 const Conversation = (props: Props) => {
   return (
     <div className='flex flex-row gap-x-2 items-start my-5' id='conversation-wrapper'>
-    <HoverCard openDelay={300} closeDelay={0}>
-      <Dialog>
-        <DialogTrigger>
-          <HoverCardTrigger className='cursor-pointer'>
-            <Image src="/baki.jpg" width={42} height={42} alt='pp' className='rounded-full'/>
-          </HoverCardTrigger>
-        </DialogTrigger>
-        <DialogContent className=' w-fit'>
-          <Suspense fallback="loading image..."><Image src="/baki.jpg" width={240} height={240} alt='pp' className='rounded-full' /></Suspense>
-        </DialogContent>
-      </Dialog>
-      <HoverCardContent className='cursor-default'>
-        Show picture
-      </HoverCardContent>
-    </HoverCard>
+      <HoverCard openDelay={300} closeDelay={0}>
+        <Dialog>
+          <DialogTrigger>
+            <HoverCardTrigger className='cursor-pointer'>
+              <Image src="/baki.jpg" width={42} height={42} alt='pp' className='rounded-full' />
+            </HoverCardTrigger>
+          </DialogTrigger>
+          <DialogContent className=' w-fit'>
+            <Suspense fallback="loading image..."><Image src="/baki.jpg" width={240} height={240} alt='pp' className='rounded-full' /></Suspense>
+          </DialogContent>
+        </Dialog>
+        <HoverCardContent className='cursor-default'>
+          Show picture
+        </HoverCardContent>
+      </HoverCard>
       <div className='flex flex-col whitespace-break-spaces' id='conversation' aria-multiline='true'>
-        <h2 className='font-semibold'>{props.username}</h2>
+        <h2 className='font-semibold'>{props.username || 'You'}</h2>
       </div>
     </div>
   )
 }
-
-Conversation.defaultProps = {username: 'You'}
 
 export default Conversation
