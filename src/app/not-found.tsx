07e1a0259f49877ee8f94d40fc19e8746a46a89c
MD <89563334+muhamaddigdaya@users.noreply.github.com>
2023-08-27 13:16:15 +0700

    404 styling

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,11 +1,15 @@
 import Link from 'next/link'
+import { ThemeProvider } from './components/ui/theme-provider'
+import { Button } from './components/ui/button'
 
 export default function NotFound() {
     return (
-        <div className='flex flex-col items-center content-center h-screen w-full bg-background'>
-            <h2 className='text-5xl'>Not Found</h2>
-            <p className='text-2xl'>Could not find requested resource</p>
-            <Link href="/" className='bg-'>Return Home</Link>
-        </div>
+        <ThemeProvider attribute="class" defaultTheme="dark" enableSystem>
+            <div className='flex min-h-screen flex-col items-center gap-y-2 p-24'>
+                <h2 className='text-5xl font-black'>404 | Not Found</h2>
+                <p className='text-2xl font-light mb-2'>The page you are looking for not exist</p>
+                <Link href="/"><Button>Return Home</Button></Link>
+            </div>
+        </ThemeProvider>
     )
 }
