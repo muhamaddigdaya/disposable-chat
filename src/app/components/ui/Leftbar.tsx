e431bddc4ede262e08296d1839c0763a2ae9958b
MD <89563334+muhamaddigdaya@users.noreply.github.com>
2023-09-01 21:23:47 +0700

    home screen button

--- src/app/components/ui/Leftbar.tsx
+++ src/app/components/ui/Leftbar.tsx
@@ -1,11 +1,13 @@
 import React from 'react'
+import { Button } from './button'
+import Link from 'next/link'
 
 type Props = {}
 
 const Leftbar = (props: Props) => {
   return (
     <div className='h-full w-1/6 bg-secondary text-secondary-foreground pl-8 pt-8'>
-
+      <Button><Link href='/'>Back to Home Screen</Link></Button>
     </div>
   )
 }
